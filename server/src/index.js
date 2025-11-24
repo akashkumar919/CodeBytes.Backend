f6e5a96c8988ccd1503148b4fb7640d4f39fff84
Akash Kumar <akashkumar975887@gmail.com>
2025-11-24 17:31:27 +0530

    changes in CORS

--- server/src/index.js
+++ server/src/index.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 
 app.use(cors({
   // origin: "*" || "http://localhost:5173",   // frontend ka URL
-  origin:"*"
-  // credentials: true
+  origin:"*",
+  credentials: true
 }));
 
 
